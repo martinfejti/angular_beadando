ab36203fef760d7c05009d6db61279a5057fc2e8
Martin <fejtimartin98@gmail.com>
2020-04-12 01:10:45 +0200

    - Create login component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RootComponent } from './root/root/root.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    RootComponent
+    RootComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
