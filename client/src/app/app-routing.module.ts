2d96ea99cf7ee30d032d950616d2e8ab60644141
Martin <fejtimartin98@gmail.com>
2020-04-12 01:18:46 +0200

    - Fix router
    Now the component page is rendered by default

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RootComponent } from './root/root/root.component';
+import { LoginComponent } from './login/login.component';
 
 
 export const AppRoutes: Routes = [
-  { path: 'root', component: RootComponent}
+  { path: '', component: LoginComponent }
 ];
 
 // donno if the export is needed above, might cause problems
 
 @NgModule({
   imports: [RouterModule.forRoot(AppRoutes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
