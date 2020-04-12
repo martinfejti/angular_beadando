2ee643260e49a963acffd2e94590606cbc6a3701
Martin <fejtimartin98@gmail.com>
2020-04-12 15:28:58 +0200

    - Implement login process
    Note: there's no db still, so of course it won't work now

--- client/src/app/login/login-service.service.ts
+++ client/src/app/login/login-service.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './../models/user.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class LoginServiceService {
 
   constructor(private httpClient: HttpClient) { }
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginServiceService as LoginService } from './login-service.service';
+import { User } from './../models/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+
+  constructor(private loginService: LoginService) {
+    this.user = new User();
+  }
+
+  validateLogin() {
+    if (this.user.username && this.user.password) {
+      this.loginService.validateLogin(this.user).subscribe(result => {
+        console.log('Result: ', result);
+      }, error => {
+        console.log('Error: ', error);
+      });
+    } else {
+      alert('Töltsön ki minden mezőt!!!');
+    }
+  }
 
   ngOnInit() {
   }
