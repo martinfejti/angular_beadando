74e23252898ec2bd28f7a03f460da814b768a83a
Martin <fejtimartin98@gmail.com>
2020-04-12 15:27:57 +0200

    - Import needed modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RootComponent } from './root/root/root.component';
@@ -12,7 +14,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [RootComponent]
