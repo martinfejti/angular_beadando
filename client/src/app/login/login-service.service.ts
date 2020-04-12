20ff13f72c325c62e0ee78d74bffe50813f3c6d2
Martin <fejtimartin98@gmail.com>
2020-04-12 14:59:19 +0200

    - Implement login service

--- client/src/app/login/login-service.service.ts
+++ client/src/app/login/login-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  validateLogin(user: User) {
+    return this.httpClient.post('/api/user/login', {
+      username: user.username,
+      password: user.password
+    });
+    // first param is the url where we post data to
+    // second param is the body
+  }
 }
