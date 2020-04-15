840129ab91432cd44aa48a1b755bb1e9625283d0
Martin <fejtimartin98@gmail.com>
2020-04-15 21:17:00 +0200

    - Implement patient list service

--- client/src/app/patient-list/patient-list.service.ts
+++ client/src/app/patient-list/patient-list.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class PatientListService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllPatients() {
+    return this.httpClient.post('http://localhost:4000/api/patient/getAllPatients', {});
+  }
 }
