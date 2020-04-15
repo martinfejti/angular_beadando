e1c84a1d8a59112c5df80cf11da917919412eb89
Martin <fejtimartin98@gmail.com>
2020-04-15 22:05:51 +0200

    - Use service in patient list component

--- client/src/app/patient-list/patient-list.component.ts
+++ client/src/app/patient-list/patient-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientListService } from './patient-list.service';
 
 @Component({
   selector: 'app-patient-list',
   templateUrl: './patient-list.component.html',
-  styleUrls: ['./patient-list.component.css']
+  styleUrls: ['./patient-list.component.css'],
+  providers: [ PatientListService ]
 })
 export class PatientListComponent implements OnInit {
 
-  constructor() { }
+  public patients: any[];
+
+  constructor(private patientListService: PatientListService) { }
 
   ngOnInit() {
+    this.getAllPatients();
+  }
+
+  getAllPatients() {
+    this.patientListService.getAllPatients().subscribe(result => {
+      this.patients = result['data'];
+    });
   }
 
 }
