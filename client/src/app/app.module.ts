e271d6e47900281ed25d962d2a464f12170f6b21
Martin <fejtimartin98@gmail.com>
2020-04-16 10:34:54 +0200

    - Generate add patient component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { RootComponent } from './root/root/root.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { PatientListComponent } from './patient-list/patient-list.component';
+import { AddPatientComponent } from './add-patient/add-patient.component';
 
 @NgModule({
   declarations: [
     RootComponent,
     LoginComponent,
     HomeComponent,
-    PatientListComponent
+    PatientListComponent,
+    AddPatientComponent
   ],
   imports: [
     BrowserModule,
