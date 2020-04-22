f6a8efdf5df4c698f73437a28bdaae46cd91576d
Martin <fejtimartin98@gmail.com>
2020-04-22 17:45:04 +0200

    - Implement edit patient function
    Also some temp style modifications

--- client/src/app/edit-patient/edit-patient.component.ts
+++ client/src/app/edit-patient/edit-patient.component.ts
@@ -11,16 +11,26 @@ import { CommonService } from './../service/common.service';
 export class EditPatientComponent implements OnInit {
 
   patient: Patient;
-  diesase: Disease;
+  disease: Disease;
+  tempPrescribedMedicines: string;
+  tempTreatmentList: string;
 
   constructor(private commonService: CommonService) {
     this.patient = new Patient();
+    this.disease = new Disease();
   }
 
   ngOnInit() {
     this.commonService.patientEditedObeservable.subscribe(result => {
       this.patient = this.commonService.patientToBeEdited;
     });
+  }
+
+  editPatient() {
+    this.disease.prescribedMedicines = this.commonService.getArrayElementsFromString(this.tempPrescribedMedicines);
+    this.disease.treatmentList = this.commonService.getArrayElementsFromString(this.tempTreatmentList);
+    console.log('disease to update: ', this.disease);
+    // call service function
   }
 
 }
